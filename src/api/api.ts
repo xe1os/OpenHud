90d1cf33fdb144412e70e9d39ecd2b8ce8093582
jwtcw7@umsl.edu <jwtcw7@umsl.edu>
2024-03-25 01:06:36 -0700

    Initial Commit
    Initial Commitv2, installing csgogsi, csgogsi-socket, and typescript.

---
+++ src/api/api.ts
@@ -0,0 +1,12 @@
+import * as I from './interface';
+
+export const port = 1337;
+export const apiURL = `http://localhost:${port}/`;
+
+const api = {
+    match: {
+
+    },
+}
+
+export default api;
