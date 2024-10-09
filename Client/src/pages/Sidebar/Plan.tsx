9438535821896b4769f09ddb52b1d4b2ef33029f
JohnTimmermann <johnwtimmermann@yahoo.com>
2024-10-09 13:44:51 -0700

    Update Plan.tsx
    text color change

--- Client/src/pages/Sidebar/Plan.tsx
+++ Client/src/pages/Sidebar/Plan.tsx
@@ -6,7 +6,7 @@ export const Plan = () => {
     <div className='flex sticky top-[calc(100vh_-_48px_-_16px)] flex-col h-12 border-t px-2 border-border justify-end text-xs'>
         <div className='flex items-center justify-between'>
             <div>
-                <p className='font-bold'>Free</p>
+                <p className='font-bold text-textcolor'>Free</p>
                 <p className='text-stone-500'>Support on Patreon</p>
             </div>
             <a href='https://www.patreon.com/JTMythic' target='_blank' className='px-2 py-1.5 font-medium bg-black hover:bg-stone-300 transition-colors rounded' rel="noreferrer"><img className='size-5' src={Patreon} alt='Patreon'/></a>
